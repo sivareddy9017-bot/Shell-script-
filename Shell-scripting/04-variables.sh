d29d92985450c197d0f8731108ff2bf7881bf29f
NANDYALA9010 <sivareddy9017@gmail.com>
2026-06-17 17:43:16 +0530

    ads

--- Shell-scripting/04-variables.sh
+++ Shell-scripting/04-variables.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 START_TIME=$(date +%s)
-
-echo "Script executed at : $TIMESTAMP"
-
+echo "Script executed at : $START_TIME"
 sleep 10
-
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "Script executed in : $TOTAL_TIME"
