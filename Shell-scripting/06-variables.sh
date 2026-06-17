e5dd9ec1810eb00a673ef1587b3380d98915a280
NANDYALA9010 <sivareddy9017@gmail.com>
2026-06-17 18:54:54 +0530

    ads

---
+++ Shell-scripting/06-variables.sh
@@ -0,0 +1,14 @@
+Num1=100
+NUM2=200
+
+SUM =$((NUM1+NUM2))
+
+echo "sum is: $sum"
+
+# Array
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
