748866b7fcc6b1015cb917baae12f30c52bc36d7
NANDYALA9010 <sivareddy9017@gmail.com>
2026-06-17 17:16:10 +0530

    ads

--- Shell-scripting/03.variables.sh
+++ Shell-scripting/03.variables.sh
@@ -4,3 +4,8 @@ echo "Enter you user name ::"
 read user_name
 
 echo "Your user name is $user_name"
+# we are not print the password just testing purpose
+echo "enter you password"
+read -s password
+
+echo "your password $password"
