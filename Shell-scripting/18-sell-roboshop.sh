0d21b24383f8525e8d28805fff8769557eb983c9
NANDYALA9010 <sivareddy9017@gmail.com>
2026-06-28 19:02:48 +0530

    added

---
+++ Shell-scripting/18-sell-roboshop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SG_ID="sg-00d83c71723801bfe" # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5"
+#ZONE_ID="Z05013202FKF0ZL12WAOP"
+#DOMAIN_NAME="daws88s.online"
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+    fi
+
+    echo "IP Address: $IP"
