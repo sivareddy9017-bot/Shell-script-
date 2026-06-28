16650559512a579dc22065b1269bc6299deaf528
NANDYALA9010 <sivareddy9017@gmail.com>
2026-06-28 21:39:08 +0530

    added

--- Shell-scripting/19-mongodb.sh
+++ Shell-scripting/19-mongodb.sh
@@ -27,15 +27,14 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying Mongo Repo" 
 
-dnf install mongodb-org -y 
-VALIDATE $? "Installing Mongodb" 
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing MongoDB server"
 
-systemctl enable mongod 
-VALIDATE $? "Enable mongodb" 
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod 
-VALIDATE $? "start mongodb" 
-
+systemctl start mongod
+VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
