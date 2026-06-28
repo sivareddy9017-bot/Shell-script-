7820b0fb338151843c819e27d577793e57cedb18
NANDYALA9010 <sivareddy9017@gmail.com>
2026-06-28 22:05:26 +0530

    added

---
+++ Shell-scripting/18.1-Instance.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SG_ID="sg-076ec9ad23dab2b28" 
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+      
+    fi
+
+    echo "IP Address: $IP"
+
+    done
